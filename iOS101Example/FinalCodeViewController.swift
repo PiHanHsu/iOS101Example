cbee4de69356e4802497a8d13a985b92c70dc2ee
PiHan Hsu <pihanhsu@gmail.com>
2016-08-10 00:58:33 +0800

    commit Final Code sample answer

--- iOS101Example/FinalCodeViewController.swift
+++ iOS101Example/FinalCodeViewController.swift
@@ -13,14 +13,32 @@ class FinalCodeViewController: UIViewController {
     @IBOutlet var inputNumberTextField: UITextField!
     @IBOutlet var resultLabel: UILabel!
     
+    @IBOutlet var playerImageView: UIImageView!
+    
     var finalCode: Int?
     var inputCode: Int?
     var topCode: Int = 0
     var bottomCode: Int = 0
     
+    var playerList = ["Alex", "Jeff", "YoMing", "PiHan", "George", "Crystal"]
+    var playerIndex = 0
+    
+    @IBOutlet var remainingNumberLable: UILabel!
+    @IBOutlet var playerNameLabel: UILabel!
+    
+    @IBOutlet var nextPlayerNameLabel: UILabel!
+    @IBOutlet var nextPlayerImageView: UIImageView!
+    
     @IBOutlet var resetButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // set up playerImageView
+        playerImageView.layer.cornerRadius = 30.0
+        playerImageView.clipsToBounds = true
+        nextPlayerImageView.layer.cornerRadius = 30.0
+        nextPlayerImageView.clipsToBounds = true
+        
         createFinalCode()
     }
     
@@ -29,15 +47,36 @@ class FinalCodeViewController: UIViewController {
         topCode = 100
         bottomCode = 0
         resetButton.enabled = false
-        resultLabel.text = "請猜1~99 任一數字"
+        resultLabel.text = "0 < 100"
+        playerIndex = Int(arc4random_uniform(UInt32(playerList.count)))
+        
+        displayPlayerInfo()
     }
     
     @IBAction func confirmButtonPressed(sender: AnyObject) {
         inputCode = Int(inputNumberTextField.text!)
+        inputNumberTextField.text = ""
         
         if inputCode == finalCode {
-            resultLabel.text = "猜中了！"
+            let title = "\(playerList[playerIndex]) 你輸了！"
+            let alert = UIAlertController(title: title, message: "請接受處罰", preferredStyle: .Alert)
+            let okAction = UIAlertAction(title: "OK", style: .Default, handler: nil)
+            let newGame = UIAlertAction(title: "New Game", style: .Default, handler: { (UIAlertAction) in
+                self.createFinalCode()
+            })
+            alert.addAction(okAction)
+            alert.addAction(newGame)
+            presentViewController(alert, animated: true, completion: nil)
+            resultLabel.text = String(finalCode!)
+            remainingNumberLable.text = "0"
             resetButton.enabled = true
+            return
+        }else if inputCode > topCode || inputCode < bottomCode {
+            let alert = UIAlertController(title: "數字超出範圍", message: "請重新輸入", preferredStyle: .Alert)
+            let okAction = UIAlertAction(title: "OK", style: .Default, handler: nil)
+            alert.addAction(okAction)
+            presentViewController(alert, animated: true, completion: nil)
+            return
         }else if inputCode > finalCode {
             topCode = inputCode!
             displayResultLabel()
@@ -46,8 +85,30 @@ class FinalCodeViewController: UIViewController {
             displayResultLabel()
         }
         
-        inputNumberTextField.text = ""
+        if playerIndex < playerList.count - 1 {
+            playerIndex += 1
+        }else {
+            playerIndex = 0
+        }
         
+        displayPlayerInfo()
+        remainingNumberLable.text = "\(topCode - bottomCode - 1)"
+
+    }
+    
+    func displayPlayerInfo() {
+        let playerName = playerList[playerIndex]
+        var nextPlayerName = ""
+        
+        playerNameLabel.text = playerName
+        playerImageView.image = UIImage(named: playerName)
+        if playerIndex == playerList.count - 1 {
+            nextPlayerName = playerList[0]
+        }else {
+            nextPlayerName = playerList[playerIndex + 1]
+        }
+        nextPlayerNameLabel.text = nextPlayerName
+        nextPlayerImageView.image = UIImage(named: nextPlayerName)
     }
     
     func displayResultLabel() {
